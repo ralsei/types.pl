3f75e308a3c5bac16325dd943076647c6b517567
JJ <git@toki.la>
2024-03-12 21:58:11 -0700

    Remove search from sidebar & mention of post search

--- app/javascript/flavours/glitch/features/compose/components/search.jsx
+++ app/javascript/flavours/glitch/features/compose/components/search.jsx
@@ -299,7 +299,7 @@ class Search extends PureComponent {
         options.push({ key: 'go-to-account', label: <FormattedMessage id='search.quick_action.go_to_account' defaultMessage='Go to profile {x}' values={{ x: <mark>@{trimmedValue.replace(/^@/, '')}</mark> }} />, action: this.handleAccountClick });
       }
 
-      const couldBeStatusSearch = searchEnabled;
+      const couldBeStatusSearch = false;
 
       if (couldBeStatusSearch && signedIn) {
         options.push({ key: 'status-search', label: <FormattedMessage id='search.quick_action.status_search' defaultMessage='Posts matching {x}' values={{ x: <mark>{trimmedValue}</mark> }} />, action: this.handleStatusSearch });
